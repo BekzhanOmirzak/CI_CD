62cd915cf2225132665c91556b6b984331ad82de
user <mail@mail.com>
2022-10-27 12:48:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ci_cd/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ci_cd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ci_cd.ui.theme.CI_CDTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			CI_CDTheme {
+				 
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String) {
+	Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+	CI_CDTheme {
+		Greeting("Android")
+	}
+}
